514043a61200fc7b3435438f0d079976045a079e
MariamKochadze <kochadzemariam80@gmail.com>
2026-05-24 20:42:08 +0400

    feat: handle Date values

--- deep-copy/index.js
+++ deep-copy/index.js
@@ -2,6 +2,10 @@ function copy(value, hash = new Map()) {
   if (value === null || typeof value !== "object") {
     return value;
   }
+    if (value instanceof Date) {
+    return new Date(value.getTime());
+  }
+
 
   return clone;
 }
