d5e18c93cff644fdf6f566bcaf0e5360abdea91c
MariamKochadze <kochadzemariam80@gmail.com>
2026-05-24 20:49:20 +0400

    feat: handle nested object, arrays, and add test

--- deep-copy/index.js
+++ deep-copy/index.js
@@ -5,7 +5,20 @@ function copy(value, hash = new Map()) {
     if (value instanceof Date) {
     return new Date(value.getTime());
   }
+    if (hash.has(value)) {
+    return hash.get(value);
+  }
+
+  const clone = Array.isArray(value) ? [] : {};
+
+  hash.set(value, clone);
+
+  for (const key of Reflect.ownKeys(value)) {
+    clone[key] = copy(value[key], hash);
+  }
 
 
   return clone;
 }
+
+module.exports = { copy };
